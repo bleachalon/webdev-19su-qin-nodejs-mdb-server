16ee7b83d17db905c8d30a444ec440ff714c22bf
bleachalon <qin.ha@husky.neu.edu>
2019-08-07 16:42:07 -0800

    minor fix

--- data/db.js
+++ data/db.js
@@ -1,4 +1,5 @@
 module.exports= function () {
+    var mongoose = require('mongoose')
     var username = process.env.MY_MONGODB_USERNAME;
     var password = process.env.MY_MONGODB_PASSWORD;
     mongoose.connect('mongodb://'+username+':'+password+'@ds127655.mlab.com:27655/heroku_6d1bnhgk', {useNewUrlParser: true});
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express')
 var app = express()
-var mongoose = require('mongoose')
 var connect = require("./data/db.js")
 
 connect()
